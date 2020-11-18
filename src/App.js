67f91b8bbace0e4070fe5430a405433fd8d10d06
unknown <forkevinsun@gmail.com>
2020-11-18 15:29:17 -0800

    1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Post from "./Post";
 import "./App.css";
 import { db, auth } from "./firebase";
 import Profile from "./Profile";
@@ -8,69 +7,11 @@ import Profile from "./Profile";
 
 
 function App() {
-  const [posts, setPosts] = useState([]);
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        //user logged in
-        console.log(authUser);
-        setUser(authUser);
-      } else {
-        //user logged out
-        setUser(null);
-      }
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, [user, username]);
-
-  // userEffect: run a piece of code based on a specific condition
-  useEffect(
-    () => {
-      //this is where code runs
-      db.collection("posts")
-        .orderBy("timestamp", "desc")
-        .onSnapshot((snapshot) => {
-          //every time a new post is added, it fires up onSnapshot
-          setPosts(
-            snapshot.docs.map((doc) => ({
-              id: doc.id,
-              post: doc.data(),
-            }))
-          );
-        });
-    },
-    [
-      // this is condition
-    ]
-  );
-
-
-
+ 
   return (
     <div className="app">
-           
       <div className="app_posts">
-        <div className="app_postLeft">
-        {posts.map(({ id, post }) => (
-          <Post
-            className="post"
-            key={id}
-            postId={id}
-            user={user}
-            username={post.username}
-            caption={post.caption}
-            imageUrl={post.imageUrl}
-          />
-        ))}
-      </div>
-
       <div className="app_profileRight">
         <Profile username={username}  />
       </div>
--- src/Profile.js
+++ src/Profile.js
@@ -5,71 +5,78 @@ import { Button } from '@material-ui/core';
 import firebase from "firebase";
 import { storage, db } from "./firebase";
 
-function Profile({username, email, imageUrl, avatarUrl}) {
-    const [avatar, setAvatar] = useState(null);
+function Profile({username, email}) {
+  const[avatarImage, setAvatarImage] = useState('')
+
+  const handleChangeAvatar = (e) => {
+    if (e.target.files[0]) {
+      setAvatarImage(e.target.files[0]);
+      console.log(e.target.files)
+    //   const uploadAvatarImage = storage.ref(`avatars/${avatarImage.name}`).put(avatarImage);
+    // uploadAvatarImage.on(
+    //     () => {
+    //     //complete function
+    //     storage
+    //       .ref("avatars")
+    //       .child(avatarImage.name)
+    //       .getDownloadURL()
+    //       .then((url) => {
+    //         //post image inside db
+    //         db.collection("posts").add({
+    //           timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    //           photoURL: url,
+             
+    //         });
+    //         setAvatarImage(null);
+    //       });
+    //   }
+    // );
+    }
+  };
+
+  // const uploadAvatar = ()=>{
+
+  //   const uploadAvatarImage = storage.ref(`avatars/${avatarImage.name}`).put(avatarImage);
+  //   uploadAvatarImage.on(
+  //       () => {
+  //       //complete function
+  //       storage
+  //         .ref("avatars")
+  //         .child(avatarImage.name)
+  //         .getDownloadURL()
+  //         .then((url) => {
+  //           //post image inside db
+  //           db.collection("posts").add({
+  //             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+  //             photoURL: url,
+             
+  //           });
+  //           setAvatarImage(null);
+  //         });
+  //     }
+  //   );
+  // } 
    
-
-    const handleChange = (e) => {
-      if (e.target.files[0]) {
-        setAvatar(e.target.files[0]);
-        }
-    };
-    const handleUploadAvatar = () => {
-          const uploadTask = storage.ref(`avatars/${avatar.name}`).put(avatar);
-          uploadTask.on(
-            "state_changed",
-             () => {
-              //complete function
-              storage
-                .ref("avatars")
-                .child(avatar.name)
-                .getDownloadURL()
-                .then((url) => {
-                  //post image inside db
-                  db.collection("avatars").add({
-                    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-                    avatarUrl: url,
-                    email: email,
-                  });
-                  setAvatar(null);
-                });
-            }
-          );
-          };
-    
-
-
-    return (
+     return (
         <div className="Profile">
            
-           <h3>USER PROFILE</h3> 
+           <h3>USER AVATAR</h3> 
            <br></br>
             <div className="profile_avatar">
-            
-            <div> <Avatar className="post_avatar" src={avatarUrl}  alt="" /></div>
-                <div className="avatarButton">
-                {/* <input
-                        type="file"
-                        onChange={handleChange} 
-                    /> */}
-                <Button
-                variant="contained"
-                color="default"
-                size="small"
-                component="label"
-                onClick={handleUploadAvatar}
-                >
-                Update Avatar
-                    <input
-                        type="file"
-                        style={{ display: "none" }}
-                        onChange={handleChange} 
-                    />
-                </Button>
-                </div>
+              <Button 
+              component="label"
+              // onClick={uploadAvatar}
+              >
+                <Avatar /> 
+                <input 
+                type="file"  
+                style={{ display: "none" }} 
+                onChange={handleChangeAvatar} />
+              </Button>
             </div> 
-             
-          
+
+         
+            
           
         </div>
     )
